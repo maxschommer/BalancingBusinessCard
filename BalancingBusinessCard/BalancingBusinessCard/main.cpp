2c733d7d3823c6eeef76c20c373ed8e141e3e8c0
Eric Miller <eric@legoaces.org>
2018-11-11 22:20:54 -0500

    Added replay code

--- BalancingBusinessCard/BalancingBusinessCard/main.cpp
+++ BalancingBusinessCard/BalancingBusinessCard/main.cpp
@@ -8,7 +8,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-const uint16_t Message[] PROGMEM = {
+const uint16_t Message[] = {
 0b111111111, // H
 0b000010000,
 0b000010000,
@@ -32,35 +32,33 @@ const uint16_t Message[] PROGMEM = {
 0b100000001,
 0b100000001,
 0b111111111,
-}
+};
 
 void ShowLine(uint16_t line) {
-    PORTA =
+    PORTA = line & 0b000001111 << 4;
+    PORTB = (line & 0b111100000 >> 1) | (line & 0b000010000 >>3);
 }
 
 int main (void)
 {
 	// set PB3 to be output
-	DDRB = 0b01111000;
+	DDRB = 0b01111010;
 	DDRA = 0b11110000;
 	while (1) {
 		
 		// flash# 1:
 		// set PB3 high
 		PORTA = 0b11110000;
-		PORTB = 0b01111000;
+		PORTB = 0b01111010;
 		_delay_ms(1000);
 		// set PB3 low
 		PORTA = 0b00000000;
 		PORTB = 0b00000000;
 		_delay_ms(1000);
 
-		// flash# 2:
-		// set PB3 high
-		PORTB = 0b00001000;
-		_delay_ms(200);
-		// set PB3 low
-		PORTB = 0b00000000;
-		_delay_ms(200);
+        for(uint8_t i = 0; i < sizeof(Message) / sizeof(Message[0]); i++){
+            ShowLine(Message[i]);
+            _delay_ms(10);
+        }
 	}
 }
